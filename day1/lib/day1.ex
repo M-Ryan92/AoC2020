bb42648220fc0e0d39d25dddf5218e0dd0a35ca1
Michel Ryan Dolstra <michel.dolstra@bettyblocks.com>
2020-12-13 17:13:11 +0100

    feat: solution to day 2

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -5,38 +5,43 @@ defmodule Day1 do
 
   def run do
     Report.get()
-    |> process()
-    |> List.first()
+    |> process(2)
   end
 
-  @spec process([integer]) :: [{integer, integer}]
-  def process(_, acc \\ [])
+  @spec process([integer], integer) :: [{integer, integer}]
+  def process(_, _, acc \\ [])
 
-  def process([number | rest], acc) do
-    case find2020(number, rest) do
-      {true, {first, second}} ->
-        process(rest, [first * second | acc])
-
-      _ ->
-        process(rest, acc)
-    end
-  end
-
-  def process([], acc), do: acc
-
-  @spec find2020(integer, [integer]) :: {true, {integer, integer}} | {false, nil}
-  def find2020(number, list) do
-    result =
-      Enum.filter(list, fn number_from_list ->
-        2020 === number + number_from_list
-      end)
-
-    case List.first(result) do
-      nil ->
-        {false, nil}
+  def process([number | rest], entries, acc) do
+    case find2020({number, number}, rest, entries) do
+      [] ->
+        process(rest, entries, acc)
 
       result ->
-        {true, {number, result}}
+        Keyword.get(result, true)
     end
+  end
+
+  def process([], _, acc), do: acc
+
+  @spec find2020({integer, [integer]}, [integer], integer) :: {boolean, integer}
+  def find2020(nil, _, _), do: {false, 0}
+
+  def find2020({2020, numbers}, _, 0), do: {true, numbers}
+  def find2020(_, _, 0), do: {false, 0}
+
+  def find2020({number, acc}, list, n) do
+    list
+    |> Enum.map(fn number_from_list ->
+      sum = number + number_from_list
+
+      if sum <= 2020 do
+        find2020({sum, number_from_list * acc}, list, n - 1)
+      else
+        {false, 0}
+      end
+    end)
+    |> List.flatten()
+    |> Enum.uniq()
+    |> Enum.filter(fn {r, _} -> r end)
   end
 end
--- day1/test/day1_test.exs
+++ day1/test/day1_test.exs
@@ -2,12 +2,20 @@ defmodule Day1Test do
   use ExUnit.Case
 
   describe "find2020" do
-    test "combination found" do
-      {true, {20, 2000}} = Day1.find2020(20, [10, 20, 300, 2000])
+    test "depth 1 combination found" do
+      assert [true: 40000] = Day1.find2020({20, 20}, [10, 20, 300, 2000], 1)
     end
 
-    test "combination not found" do
-      {false, nil} = Day1.find2020(20, [10, 20, 300])
+    test "depth 1 combination not found" do
+      assert [] = Day1.find2020({20, 20}, [10, 20, 300], 1)
+    end
+
+    test "depth 2 combination found" do
+      assert [true: 398_000] = Day1.find2020({20, 20}, [10, 20, 300, 1990], 2)
+    end
+
+    test "depth 2 combination not found" do
+      assert [] = Day1.find2020({20, 20}, [10, 20, 300], 2)
     end
   end
 end
